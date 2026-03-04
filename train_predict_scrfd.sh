b3958650e02ac857ec2669d9e276613b06b9386a
Jan108 <jan@thebraker.net>
2026-03-04 15:23:39 +0100

    tested and fixed training

--- train_predict_scrfd.sh
+++ train_predict_scrfd.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-for scrfd_type in "25" "10" "34"; do
+for scrfd_type in "2.5" "10" "34"; do
   for cls in "all" "bird" "cat" "cat_like" "dog" "dog_like" "horse_like" "small_animals"; do
     PYTHONPATH='/mnt/data/afarec/code/face_detection/SCRFD/':$PYTHONPATH \
     python /mnt/data/afarec/code/face_detection/SCRFD/tools/train.py \
-      "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_${scrfd_type}_${cls}.py" \
+      "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_${scrfd_type}g_${cls}.py" \
       --seed 0 \
       --work-dir "./work_dir/scrfd_${scrfd_type}_${cls}/"
 
     PYTHONPATH='/mnt/data/afarec/code/face_detection/SCRFD/':$PYTHONPATH \
     python /mnt/data/afarec/code/face_detection/SCRFD/tools/test_widerface.py \
-      "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_${cls}.py" \
+      "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_${scrfd_type}g_${cls}.py" \
       "./work_dir/scrfd_${scrfd_type}_${cls}/weights/lastest.pth" \
       --mode 2 \
       --save-preds \
@@ -20,7 +20,7 @@ for scrfd_type in "25" "10" "34"; do
 
   PYTHONPATH='/mnt/data/afarec/code/face_detection/SCRFD/':$PYTHONPATH \
   python /mnt/data/afarec/code/face_detection/SCRFD/tools/test_widerface.py \
-    "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_all.py" \
+    "$(dirname "$0")/configs/scrfd_${scrfd_type}/scrfd_${scrfd_type}g_all.py" \
     "$(dirname "$0")/weights/model_pretrained_${scrfd_type}GF.pth" \
     --mode 2 \
     --save-preds \
